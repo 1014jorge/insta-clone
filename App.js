64ae7e78633fb1095c0dc3abbd5c70fdb9f82ad4
Jorge Gonzalez <jorge.1014@gmail.com>
2024-01-29 20:30:12 -0800

    installed firebase

--- App.js
+++ App.js
@@ -1,5 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+
+import * as firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAYw6F7OK3Piy1tV9gqVlmosPmOUzfEF7g",
+  authDomain: "insta-clone-9a6a2.firebaseapp.com",
+  projectId: "insta-clone-9a6a2",
+  storageBucket: "insta-clone-9a6a2.appspot.com",
+  messagingSenderId: "263071350712",
+  appId: "1:263071350712:web:98ab955b451dc8a2bd194d",
+  measurementId: "G-9M6WP2FF0P"
+};
+
+if(firebase.apps.length === 0){
+  firebase.initializeApp()
+}
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LandingScreen from './my-app/components/auth/Landing';
